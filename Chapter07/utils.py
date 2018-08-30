2adcce0b3afca1ccab1306622a29a95a866aad04
MSalvaris <msalvaris@users.noreply.github.com>
2018-08-30 14:27:19 +0100

    Initial commit

---
+++ Chapter07/utils.py
@@ -0,0 +1,76 @@
+import subprocess
+import os
+import sys
+import glob
+import json
+import numpy as np
+import time
+from ast import literal_eval
+import matplotlib.pyplot as plt
+from bokeh.plotting import figure
+from bokeh.io import show, export_svgs
+from IPython.display import SVG, display
+
+
+def get_number_processors():
+    """Get the number of processors in a CPU."""
+    try:
+        num = os.cpu_count()
+    except Exception:
+        # force exception in case mutiprocessing is not installed
+        import multiprocessing
+        num = multiprocessing.cpu_count()
+    return num
+
+
+def to_1dimension(df, step_size):
+    """Transform to LSTM time series format"""
+    X, y = [], []
+    for i in range(len(df)-step_size-1):
+        data = df[i:(i+step_size), 0]
+        X.append(data)
+        y.append(df[i + step_size, 0])
+    X, y = np.array(X), np.array(y)
+    X = np.reshape(X, (X.shape[0], 1, X.shape[1]))
+    return X, y
+
+
+def plot_series(values, xlabel=None, ylabel=None, color='b', legend=None):
+    """General function to plot series"""
+    xx = np.arange(1, len(values) + 1, 1)
+    plt.plot(xx, values, color, label=legend)
+    plt.legend(loc='upper left')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
+
+
+def plot_series_prediction(true_values, train_predict, test_predict,
+                           time_ahead=1, title=None, xlabel=None, ylabel=None,
+                           color=['green', 'red', 'blue'],
+                           legend=[None, None, None]):
+    """Plot true values, train prediciton and test prediction with bokeh"""
+    TOOLS = 'pan,wheel_zoom,box_zoom,reset,save,box_select'
+    # x axis
+    xx = np.array(range(true_values.shape[0]))
+    xx1 = np.array(range(time_ahead, len(train_predict)+time_ahead))
+    xx2 = np.array(
+        range(len(train_predict)+(time_ahead*2)+1, len(true_values)-1))
+
+    # figure
+    p = figure(title=title, tools=TOOLS)
+    p.line(xx, true_values.squeeze(),
+           legend=legend[0], line_color=color[0], line_width=2)
+    p.line(xx1, train_predict.squeeze(),
+           legend=legend[1], line_color=color[1], line_width=1)
+    p.line(xx2, test_predict.squeeze(),
+           legend=legend[2], line_color=color[2], line_width=1)
+    p.axis[0].axis_label = xlabel
+    p.axis[1].axis_label = ylabel
+    p.legend.location = "top_left"
+    show(p)
+
+    # save to svg so it is rendered on github
+    p.output_backend = "svg"
+    export_svgs(p, filename=title + '_.svg')
+    display(SVG(title + '_.svg'))
--- README.md
+++ README.md
@@ -1,11 +1,42 @@
 # Apress Source Code
 
-This repository accompanies [*Deep Learning with Azure*](https://www.apress.com/9781484236789) by Mathew Salvaris, Danielle Dean, Wee Hyong Tok (Apress, 2018).
+This repository accompanies [*Deep Learning with Azure*](https://www.apress.com/9781484236789) by Mathew Salvaris, Danielle Dean, Wee Hyong Tok (Apress, 2018). It contains the notebook examples from the book. The notebooks are broken up into their respective chapters.
+
+* [Chapter 6](Chapter06): Convolutional Neural Networks
+* [Chapter 7](Chapter07): Recurrent Neural Networks
+* [Chapter 9](Chapter09): Training AI models
 
 [comment]: #cover
-![Cover image](9781484236789.jpg)
+![Cover image](images/9781484236789.jpg)
 
 Download the files as a zip using the green button, or clone the repository to your machine using Git.
+
+## Prerequisites
+* PC or VM with Linux (Ubuntu). We suggest using an [Azure Linux DSVM](https://azuremarketplace.microsoft.com/en-gb/marketplace/apps/microsoft-ads.linux-data-science-vm-ubuntu)
+* Basic knowledge of [anaconda environments](https://conda.io/docs/user-guide/tasks/manage-environments.html)
+* Basic knowledge of [Jupyter notebooks](https://jupyter-notebook-beginner-guide.readthedocs.io/en/latest/what_is_jupyter.html)
+
+## Setup
+Please create a conda environment that will satisfy all the Python dependencies required by the notebooks. We have supplied a YAML file with the necessary dependencies [here](environment.yml)
+
+You can create a conda environment using the command below:
+```bash
+conda env create -f environment.yml
+```
+
+Once the environment has been created successfully activate it:
+```bash
+source activate azureai
+```
+
+With the environment activated start the Jupyter notebook server:
+```bash
+jupyter notebook --ip=* --port=9999 --no-browser
+```
+
+The above command tells Jupyter notebook to accept connections from all IPs and listen on port 9999. If you are using a different port or wish to restrict the IPs adjust accordingly.
+
+To access the Jupyter notebook simply point your web-browser to the appropriate IP on port 9999
 
 ## Releases
 
@@ -13,4 +44,4 @@ Release v1.0 corresponds to the code in the published book, without corrections 
 
 ## Contributions
 
-See the file Contributing.md for more information on how you can contribute to this repository.
+See the file [Contributing.md](Contributing.md) for more information on how you can contribute to this repository.
